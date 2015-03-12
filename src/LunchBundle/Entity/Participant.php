545579f39d1269ea6900b789440d0212ff8e310b
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-03-12 21:38:11 +0100

    Participant Entity

---
+++ src/LunchBundle/Entity/Participant.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace LunchBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Participant
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="LunchBundle\Entity\ParticipantRepository")
+ */
+class Participant
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return Participant
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
