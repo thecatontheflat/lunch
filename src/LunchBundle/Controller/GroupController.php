2ca9099e6fcf9c2282ee90909a2f8f95f10bf148
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-03-13 00:08:50 +0100

    Randomizing groups

--- src/LunchBundle/Controller/GroupController.php
+++ src/LunchBundle/Controller/GroupController.php
@@ -8,6 +8,26 @@ class GroupController extends Controller
 {
     public function listAction()
     {
-        return $this->render('LunchBundle:Group:list.html.twig', []);
+        $participants = $this->getDoctrine()
+            ->getRepository('LunchBundle:Participant')
+            ->findAll();
+
+        shuffle($participants);
+
+        $groups = array_chunk($participants, 4);
+
+        $lastGroup = end($groups);
+        if (count($lastGroup) < 3) {
+            $lastGroup = array_pop($groups);
+            $i = 0;
+            while ($lastGroup) {
+                array_push($groups[$i], array_pop($lastGroup));
+                $i++;
+            }
+        }
+
+        return $this->render('LunchBundle:Group:list.html.twig', [
+            'groups' => $groups
+        ]);
     }
 }
